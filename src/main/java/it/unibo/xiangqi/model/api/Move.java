0f82455fb12675908426c4173a7920d8a61e7bbf
Shenghui Hong <shenghui.hong@studio.unibo.it>
2026-05-31 18:15:19 +0200

    add Move interface

--- src/main/java/it/unibo/xiangqi/model/api/Move.java
+++ src/main/java/it/unibo/xiangqi/model/api/Move.java
@@ -1,5 +1,39 @@
 package it.unibo.xiangqi.model.api;
 
 public interface Move {
-    
+
+    /**
+     * Returns the starting position of the move.
+     * 
+     * @return the position where the piece moves from
+     */
+    Position getFrom();
+
+    /**
+     * Returns the destination position of the move.
+     * 
+     * @return the position where the piece moves to
+     */
+    Position getTo();
+
+    /**
+     * Returns the piece that was captured during this move, if any.
+     * 
+     * @return the captured piece, or null if no piece was captured
+     */
+    Piece getCapturedPiece();
+
+    /**
+     * Returns the piece that is performing this move.
+     *
+     * @return the moving piece
+     */
+    Piece getPiece();
+
+    /**
+     * Returns whether this move captures an enemy piece.
+     *
+     * @return true if a piece is captured, false otherwise
+     */
+    boolean isCapture();
 }
