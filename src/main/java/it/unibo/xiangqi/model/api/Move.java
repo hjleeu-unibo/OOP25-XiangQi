0b9e014a15a4961dff443459c1f25f8ac379fc7a
Shenghui Hong <shenghui.hong@studio.unibo.it>
2026-06-01 13:30:06 +0200

    Update Move interface, deleting 2 functions: getCapturedPiece() and isCapture

--- src/main/java/it/unibo/xiangqi/model/api/Move.java
+++ src/main/java/it/unibo/xiangqi/model/api/Move.java
@@ -7,33 +7,19 @@ public interface Move {
      * 
      * @return the position where the piece moves from
      */
-    Position getFrom();
+    public Position getFrom();
 
     /**
      * Returns the destination position of the move.
      * 
      * @return the position where the piece moves to
      */
-    Position getTo();
-
-    /**
-     * Returns the piece that was captured during this move, if any.
-     * 
-     * @return the captured piece, or null if no piece was captured
-     */
-    Piece getCapturedPiece();
+    public Position getTo();
 
     /**
      * Returns the piece that is performing this move.
      *
      * @return the moving piece
      */
-    Piece getPiece();
-
-    /**
-     * Returns whether this move captures an enemy piece.
-     *
-     * @return true if a piece is captured, false otherwise
-     */
-    boolean isCapture();
+    public Piece getPiece();
 }
