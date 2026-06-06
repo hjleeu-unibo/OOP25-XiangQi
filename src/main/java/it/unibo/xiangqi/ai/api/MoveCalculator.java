154835f4cd188cbca547b2a78ffba75bf2d41a7e
hjleeu-unibo <haojie.liu@studio.unibo.it>
2026-06-06 14:26:06 +0200

    Haojie-Liu | Setup AI interface

--- src/main/java/it/unibo/xiangqi/ai/api/MoveCalculator.java
+++ src/main/java/it/unibo/xiangqi/ai/api/MoveCalculator.java
@@ -1,5 +1,21 @@
 package it.unibo.xiangqi.ai.api;
 
+import it.unibo.xiangqi.model.api.Board;
+import it.unibo.xiangqi.model.api.GameState;
+import it.unibo.xiangqi.model.api.Move;
+
 public interface MoveCalculator {
-    
+    /**
+     * Calculate the score of the board at that moment.
+     * @ gm State of the board, as a simulation of the game.
+     * @return Score of the board.
+     */
+    public double calculateBoardScore(GameState gm);
+
+    /**
+     * Return the best Move for the current player.
+     * @param board Game Board.
+     * @return The Best Move.
+     */
+    public Move getBestMove(Board board);
 }
---
+++ src/main/java/it/unibo/xiangqi/ai/impl/MoveCalculatorImpl.java
@@ -0,0 +1,16 @@
+package it.unibo.xiangqi.ai.impl;
+
+import it.unibo.xiangqi.ai.api.MoveCalculator;
+import it.unibo.xiangqi.model.api.Board;
+import it.unibo.xiangqi.model.api.GameState;
+import it.unibo.xiangqi.model.api.Move;
+
+public class MoveCalculatorImpl implements MoveCalculator {
+    public double calculateBoardScore(GameState gm) {
+        return 0;
+    }
+
+    public Move getBestMove(Board board) {
+        return null;
+    }
+}
