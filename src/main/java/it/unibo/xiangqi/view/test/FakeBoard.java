120d8f82434e8109e78fcc6904632b070fcd29c6
Chabraoui Mansour <mansour.chabraoui@studio.unibo.it>
2026-06-18 16:22:24 +0200

    changes to folder structure

--- src/main/java/it/unibo/xiangqi/view/test/FakeBoard.java
+++ src/main/java/it/unibo/xiangqi/view/test/FakeBoard.java
@@ -2,7 +2,7 @@ package it.unibo.xiangqi.view.test;
 
 import java.util.List;
 
-import it.unibo.xiangqi.common.api.Position;
+import it.unibo.xiangqi.common.Position;
 import it.unibo.xiangqi.model.api.Board;
 import it.unibo.xiangqi.model.api.Piece;
 
