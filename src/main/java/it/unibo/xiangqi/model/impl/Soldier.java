66f6bd377e434287aef0bab86c83045c70a7298d
Shenghui Hong <shenghui.hong@studio.unibo.it>
2026-06-18 23:50:31 +0200

    Update Soldier piece class

--- src/main/java/it/unibo/xiangqi/model/impl/Soldier.java
+++ src/main/java/it/unibo/xiangqi/model/impl/Soldier.java
@@ -1,5 +1,6 @@
 package it.unibo.xiangqi.model.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import it.unibo.xiangqi.common.api.PieceType;
@@ -11,19 +12,18 @@ import it.unibo.xiangqi.model.api.Position;
 
 public class Soldier extends AbstractPiece{
 
-    protected Soldier(PieceType type, Player owner, Position position, int value) {
+    protected Soldier(PieceType type, Player owner, Position position, double value) {
         super(type, owner, position, value);
     }
 
     @Override
     public Piece createPiece(PieceType type, Player owner, Position startingPosition) {
-        return new Soldier(type, owner, startingPosition, 10);
+        return new Soldier(type, owner, startingPosition, 10.0);
     }
 
     @Override
     public List<Move> getMoves(Board board) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getMoves'");
+        List<Move> moves = new ArrayList<>();
+        return moves;
     }
-
 }
