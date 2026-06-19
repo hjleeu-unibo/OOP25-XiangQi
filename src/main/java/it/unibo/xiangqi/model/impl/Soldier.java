ac1b0a602f0e6a23a3adc57915322e7e942bb28c
Shenghui Hong <shenghui.hong@studio.unibo.it>
2026-06-19 14:53:00 +0200

    Update Soldier class, add new private method tryAddMove

--- src/main/java/it/unibo/xiangqi/model/impl/Soldier.java
+++ src/main/java/it/unibo/xiangqi/model/impl/Soldier.java
@@ -36,10 +36,33 @@ public class Soldier extends AbstractPiece{
 
         // Forward direction: RED moves toward increasing rows, BLACK toward decreasing rows
         final int forward = isRed ? 1 : -1;
+        tryAddMove(moves, board, current, row + forward, col);
+
+        // Sideways moves are only allowed after crossing the river
+        if (hasCrossedRiver(row, isRed)) {
+            tryAddMove(moves, board, current, row, col + 1);
+            tryAddMove(moves, board, current, row, col - 1);
+        }
         return moves;
     }
 
     private boolean hasCrossedRiver(final int row, final boolean isRed) {
         return isRed ? row > RIVER_ROW_RED : row < RIVER_ROW_BLACK;
+    }
+
+    private void tryAddMove(final List<Move> moves, final Board board,
+            final Position from, final int toRow, final int toCol) {
+        // Manual bounds check before creating the Position
+        if (toRow < 0 || toRow >= Position.ROWS || toCol < 0 || toCol >= Position.COLS) {
+            return;
+        }
+
+        final Position to = new Position(toRow, toCol);
+        final Piece target = board.getPieceAt(to);
+
+        // Target cell must be empty or occupied by an enemy piece
+        if (target == null || !target.getOwner().equals(this.getOwner())) {
+            moves.add(new MoveImpl(from, to, this));
+        }
     }
 }
