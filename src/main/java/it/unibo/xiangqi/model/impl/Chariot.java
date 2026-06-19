b1e13da17aa44f62e41e3fb62cac6def9dff0933
Shenghui Hong <shenghui.hong@studio.unibo.it>
2026-06-19 13:06:29 +0200

    Add Chariot class

---
+++ src/main/java/it/unibo/xiangqi/model/impl/Chariot.java
@@ -0,0 +1,32 @@
+package it.unibo.xiangqi.model.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import it.unibo.xiangqi.common.api.PieceType;
+import it.unibo.xiangqi.model.api.Board;
+import it.unibo.xiangqi.model.api.Move;
+import it.unibo.xiangqi.model.api.Piece;
+import it.unibo.xiangqi.model.api.Player;
+import it.unibo.xiangqi.model.api.Position;
+
+public class Chariot extends AbstractPiece{
+
+    private static final int VALUE = 90;
+
+    protected Chariot(PieceType type, Player owner, Position position, int initialvalue) {
+        super(PieceType.CHARIOT, owner, position, VALUE);
+    }
+
+    @Override
+    public Piece createPiece(PieceType type, Player owner, Position startingPosition) {
+        return null;
+    }
+
+    @Override
+    public List<Move> getMoves(Board board) {
+        List<Move> moves = new ArrayList<>();
+        return moves;
+    }
+
+}
