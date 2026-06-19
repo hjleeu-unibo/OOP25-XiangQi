c29a63208a404ed93fca3fa4e38c573551c7f36a
Shenghui Hong <shenghui.hong@studio.unibo.it>
2026-06-20 00:28:25 +0200

    Update Horse class, deleting the method createPiece

--- src/main/java/it/unibo/xiangqi/model/impl/Horse.java
+++ src/main/java/it/unibo/xiangqi/model/impl/Horse.java
@@ -6,7 +6,6 @@ import java.util.List;
 import it.unibo.xiangqi.common.api.PieceType;
 import it.unibo.xiangqi.model.api.Board;
 import it.unibo.xiangqi.model.api.Move;
-import it.unibo.xiangqi.model.api.Piece;
 import it.unibo.xiangqi.model.api.Player;
 import it.unibo.xiangqi.model.api.Position;
 
@@ -14,13 +13,8 @@ public class Horse extends AbstractPiece {
 
     private static final int VALUE = 40;
 
-    protected Horse(PieceType type, Player owner, Position position, int initialvalue) {
+    protected Horse(Player owner, Position position) {
         super(PieceType.HORSE, owner, position, VALUE);
-    }
-
-    @Override
-    public Piece createPiece(PieceType type, Player owner, Position startingPosition) {
-        return null;
     }
 
     @Override
@@ -28,5 +22,7 @@ public class Horse extends AbstractPiece {
         List<Move> moves = new ArrayList<>();
         return moves;
     }
+
+
 
 }
