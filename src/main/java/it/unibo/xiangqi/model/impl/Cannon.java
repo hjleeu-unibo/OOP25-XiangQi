247320755fcbba378b1b524347ad953ba3b65944
Shenghui Hong <shenghui.hong@studio.unibo.it>
2026-06-22 23:16:35 +0200

    Update Cannon class, define the Directions for move

--- src/main/java/it/unibo/xiangqi/model/impl/Cannon.java
+++ src/main/java/it/unibo/xiangqi/model/impl/Cannon.java
@@ -6,6 +6,7 @@ import java.util.List;
 import it.unibo.xiangqi.common.api.PieceType;
 import it.unibo.xiangqi.model.api.Board;
 import it.unibo.xiangqi.model.api.Move;
+import it.unibo.xiangqi.model.api.Piece;
 import it.unibo.xiangqi.model.api.Player;
 import it.unibo.xiangqi.model.api.Position;
 
@@ -13,14 +14,23 @@ public class Cannon extends AbstractPiece{
 
     private static final int VALUE = 45;
 
+    // Up, down, left, right
+    private static final int[][] DIRECTIONS = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
     protected Cannon(Player owner, Position position) {
         super(PieceType.CANNON, owner, position, VALUE);
     }
 
     @Override
-    public List<Move> getMoves(Board board) {
-        List<Move> moves = new ArrayList<>();
+    public List<Move> getMoves(final Board board) {
+        final List<Move> moves = new ArrayList<>();
+        final Position current = getPosition();
+
+        for (final int[] dir : DIRECTIONS) {
+            int row = current.getRow() + dir[0];
+            int col = current.getCol() + dir[1];
+            boolean screenFound = false; // has the cannon platform been found?
+        }
         return moves;
     }
-
 }
