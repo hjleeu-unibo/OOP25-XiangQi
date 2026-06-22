472df9e5b4c17534d72c165ac714c969c736977f
Shenghui Hong <shenghui.hong@studio.unibo.it>
2026-06-22 15:02:10 +0200

    Update Chariot class, writing some comments

--- src/main/java/it/unibo/xiangqi/model/impl/Chariot.java
+++ src/main/java/it/unibo/xiangqi/model/impl/Chariot.java
@@ -14,7 +14,7 @@ public class Chariot extends AbstractPiece{
     private static final int VALUE = 90;
 
     // Up, down, left, right
-    private static final position DIRECTIONS = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    private static final int[][] DIRECTIONS = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
     protected Chariot(Player owner, Position position) {
         super(PieceType.CHARIOT, owner, position, VALUE);
@@ -24,10 +24,15 @@ public class Chariot extends AbstractPiece{
     public List<Move> getMoves(final Board board) {
         final List<Move> moves = new ArrayList<>();
         final Position current = this.getPosition();
-
+        // Explore each orthogonal direction (up, down, left, right)
         for (final int[] dir : DIRECTIONS) {
             int row = current.getRow() + dir[0];
             int col = current.getCol() + dir[1];
+            /*
+            Continue moving in the same direction until blocked,
+            when the block is occupied by Enemy -> capture and stop
+            when the block is occupiede by Friendly -> stop
+            */ 
             while (tryAddMove(moves, board, current, row, col)) {
                 row += dir[0];
                 col += dir[1];
