36a9fa80a3bbe22a41fd61663a087512ef875e64
hjleeu-unibo <haojie.liu@studio.unibo.it>
2026-06-22 12:40:04 +0200

    Haojie-Liu | Change threatening factor to 1.5

--- src/main/java/it/unibo/xiangqi/ai/impl/MoveCalculatorImpl.java
+++ src/main/java/it/unibo/xiangqi/ai/impl/MoveCalculatorImpl.java
@@ -29,7 +29,7 @@ package it.unibo.xiangqi.ai.impl;
  * Above, these are the value of each piece during the game.
  * But they can get some extra value:
  *  - when they are protecting some ally pieces: own_value + ally_pieces_values
- *  - when they are threatening some enemy pieces: own_value + 15 x enemy_pieces_value
+ *  - when they are threatening some enemy pieces: own_value + 1.5 x enemy_pieces_value
  * With these logics, this system will prefer attack instead of protect, make the game flow faster.
  */
 
@@ -126,7 +126,7 @@ public class MoveCalculatorImpl implements MoveCalculator {
                 }
 
                 if (!isProtected) {
-                    newValue += 15 * capturedPiece.getInitialValue();
+                    newValue += 1.5 * capturedPiece.getInitialValue();
                 }
             }
         }
