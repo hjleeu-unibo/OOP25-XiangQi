390e03756814d20ef5d8195fa4ecd0c84b31316f
hjleeu-unibo <haojie.liu@studio.unibo.it>
2026-06-26 23:10:01 +0200

    Update GameState

--- src/main/java/it/unibo/xiangqi/model/api/GameState.java
+++ src/main/java/it/unibo/xiangqi/model/api/GameState.java
@@ -7,9 +7,9 @@ import it.unibo.xiangqi.model.impl.GameStateImpl;
 
 public interface GameState {
     static GameState createGameState(Board board, List<Player> players, Player currentPlayer){
-        return new GameStateImpl(board, players.get(0), players.get(1), currentPlayer);
+        return new GameStateImpl(board, players.get(0), players.get(1), currentPlayer); 
     }
     Board getBoard(); 
-    Player getCurrentPlayer();
-    void applyMove(Move move);
+    Player getCurrentPlayer(); 
+    GameState applyMove(Move move); 
 }
