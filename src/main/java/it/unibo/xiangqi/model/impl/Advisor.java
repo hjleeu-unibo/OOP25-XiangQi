dd59fbd5928398df4b5b4075dee04b7faa8c66c1
Shenghui Hong <shenghui.hong@studio.unibo.it>
2026-06-26 16:15:09 +0200

    Update Advisor class

--- src/main/java/it/unibo/xiangqi/model/impl/Advisor.java
+++ src/main/java/it/unibo/xiangqi/model/impl/Advisor.java
@@ -3,6 +3,7 @@ package it.unibo.xiangqi.model.impl;
 import java.util.ArrayList;
 import java.util.List;
 
+import it.unibo.xiangqi.common.api.Color;
 import it.unibo.xiangqi.common.api.PieceType;
 import it.unibo.xiangqi.model.api.Board;
 import it.unibo.xiangqi.model.api.Move;
@@ -15,8 +16,6 @@ public class Advisor extends AbstractPiece {
     private static final int PALACE_COL_MIN = 3;
     private static final int PALACE_COL_MAX = 5;
     private static final int PALACE_ROW_RED_MIN = 7;  // red palace: rows 7-9
-    private static final int PALACE_ROW_RED_MAX = 9;
-    private static final int PALACE_ROW_BLACK_MIN = 0;
     private static final int PALACE_ROW_BLACK_MAX = 2;  // black palace: rows 0-2
     private static final int[][] DIRECTIONS = {{1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
 
@@ -32,6 +31,21 @@ public class Advisor extends AbstractPiece {
     @Override
     public List<Move> getMoves(Board board) {
        List<Move> moves = new ArrayList<>();
+       final Position current = getPosition();
+       final int row = current.getRow();
+       final int col = current.getCol();
+       final boolean isRed = getOwner().getColor() == Color.RED;
+       
+       for (final int[] dir : DIRECTIONS) {
+            final int toRow = row + dir[0];
+            final int toCol = col + dir[1];
+
+            // the Advisor must stay within the Palace
+            if ((isRed ? toRow >= PALACE_ROW_RED_MIN : toRow <= PALACE_ROW_BLACK_MAX)
+                && toCol >= PALACE_COL_MIN && toCol <= PALACE_COL_MAX) {
+                    tryAddMove(moves, board, current, toRow, toCol);
+            }
+       }
        return moves;
     }
 
