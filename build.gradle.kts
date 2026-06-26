be617492732beae45e86ea8a19c21ac9dd706584
hjleeu-unibo <haojie.liu@studio.unibo.it>
2026-06-26 17:29:10 +0200

    Update build.gradle.kts to enable testing

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,10 +5,17 @@ repositories { // Where to search for dependencies
 }
 
 dependencies {
-        // JUnit API and testing engine
-        testImplementation(platform("org.junit:junit-bom:6.0.3"))
-        testImplementation("org.junit.jupiter:junit-jupiter-api")
-        testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-        testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    // JUnit API and testing engine
+    testImplementation(platform("org.junit:junit-bom:6.0.3"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+
+    // Mockito
+    testImplementation("org.mockito:mockito-core:5.15.2")
+    testRuntimeOnly("org.mockito:mockito-junit-jupiter:5.15.2")
 }
 
+tasks.test {
+    useJUnitPlatform()
+}
