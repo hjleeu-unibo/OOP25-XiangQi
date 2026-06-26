7da5862ba7004f4d1aa45d8715b659ce8bd1b689
Shenghui Hong <shenghui.hong@studio.unibo.it>
2026-06-26 15:41:01 +0200

    Update Elephant class: define the river for Red and Black, chose the directions updating the row and col of position

--- src/main/java/it/unibo/xiangqi/model/impl/Elephant.java
+++ src/main/java/it/unibo/xiangqi/model/impl/Elephant.java
@@ -3,6 +3,7 @@ package it.unibo.xiangqi.model.impl;
 import java.util.ArrayList;
 import java.util.List;
 
+import it.unibo.xiangqi.common.api.Color;
 import it.unibo.xiangqi.common.api.PieceType;
 import it.unibo.xiangqi.model.api.Board;
 import it.unibo.xiangqi.model.api.Move;
@@ -12,7 +13,8 @@ import it.unibo.xiangqi.model.api.Position;
 public class Elephant extends AbstractPiece {
 
     private static final int VALUE = 20;
-
+    private static final int RIVER_ROW_RED = 4;
+    private static final int RIVER_ROW_BLACK = 5;
     private static final int[][] DIRECTIONS = {{2, 2}, {2, -2}, {-2, 2}, {-2, -2}};
 
     protected Elephant(Player owner, Position position) {
@@ -27,7 +29,17 @@ public class Elephant extends AbstractPiece {
     @Override
     public List<Move> getMoves(Board board) {
         List<Move> moves = new ArrayList<>();
+        final Position current = getPosition();
+        final int row = current.getRow();
+        final int col = current.getCol();
+        final boolean isRed = getOwner().getColor() == Color.RED;
+
+        for (final int[] dir : DIRECTIONS) {
+            final int toRow = row + dir[0];
+            final int toCol = col + dir[1];
+            final int eyeRow = row + dir[0] / 2;
+            final int eyeCol = col + dir[1] / 2;
+        }
         return moves;
     }
-
 }
