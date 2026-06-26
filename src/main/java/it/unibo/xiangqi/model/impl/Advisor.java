3250d4048901443c579fedc96729e38eb9585776
Shenghui Hong <shenghui.hong@studio.unibo.it>
2026-06-26 16:07:01 +0200

    Update Advisor class, defining the directions to move and the PALACE

--- src/main/java/it/unibo/xiangqi/model/impl/Advisor.java
+++ src/main/java/it/unibo/xiangqi/model/impl/Advisor.java
@@ -12,6 +12,13 @@ import it.unibo.xiangqi.model.api.Position;
 public class Advisor extends AbstractPiece {
 
     private static final int VALUE = 20;
+    private static final int PALACE_COL_MIN = 3;
+    private static final int PALACE_COL_MAX = 5;
+    private static final int PALACE_ROW_RED_MIN = 7;  // red palace: rows 7-9
+    private static final int PALACE_ROW_RED_MAX = 9;
+    private static final int PALACE_ROW_BLACK_MIN = 0;
+    private static final int PALACE_ROW_BLACK_MAX = 2;  // black palace: rows 0-2
+    private static final int[][] DIRECTIONS = {{1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
 
     protected Advisor(Player owner, Position position) {
         super(PieceType.ADVISOR, owner, position, VALUE);
