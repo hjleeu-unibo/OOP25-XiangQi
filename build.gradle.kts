4a6127caec98004fad92cd4769c0f415d55fbe6a
hjleeu-unibo <haojie.liu@studio.unibo.it>
2026-06-26 16:59:20 +0200

    Haojie-Liu | Fix build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ repositories { // Where to search for dependencies
 dependencies {
     // JUnit API and testing engine
     testImplementation(platform("org.junit:junit-bom:6.0.3"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 
@@ -15,4 +15,3 @@ dependencies {
     testImplementation("org.mockito:mockito-core:5.15.2")
     testRuntimeOnly("org.mockito:mockito-junit-jupiter:5.15.2")
 }
-
