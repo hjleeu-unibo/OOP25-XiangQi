43c0abe435af23015ca1e257ed8bacfe6802edf7
Shenghui Hong <shenghui.hong@studio.unibo.it>
2026-06-27 13:17:00 +0200

    Update Generale class, defining the Palace and the directions to move

--- src/main/java/it/unibo/xiangqi/model/impl/General.java
+++ src/main/java/it/unibo/xiangqi/model/impl/General.java
@@ -13,6 +13,18 @@ public class General extends AbstractPiece{
 
     private static final int VALUE = 1000;
 
+    // the palace spans columns 3-5 for both sides
+    private static final int PALACE_COL_MIN = 3;
+    private static final int PALACE_COL_MAX = 5;
+
+    // red palace: rows 7-9, black palace: rows 0-2
+    // the remaining bounds (red max row 9, black min row 0) are covered by tryAddMove
+    private static final int PALACE_ROW_RED_MIN = 7;
+    private static final int PALACE_ROW_BLACK_MAX = 2;
+
+    // 4 orthogonal moves of magnitude 1 (no diagonal, unlike Advisor)
+    private static final int[][] DIRECTIONS = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
     protected General(Player owner, Position position) {
         super(PieceType.GENERAL, owner, position, VALUE);
     }
