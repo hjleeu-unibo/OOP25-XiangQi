504ea251d6d9c40be7be9f024e8c85b500a16a40
hjleeu-unibo <haojie.liu@studio.unibo.it>
2026-06-28 01:30:33 +0200

    Adjust import path

--- src/main/java/it/unibo/xiangqi/view/api/GameView.java
+++ src/main/java/it/unibo/xiangqi/view/api/GameView.java
@@ -2,9 +2,9 @@ package it.unibo.xiangqi.view.api;
 
 import java.util.List;
 
-import it.unibo.xiangqi.common.Color;
-import it.unibo.xiangqi.common.Move;
-import it.unibo.xiangqi.common.Position;
+import it.unibo.xiangqi.common.api.Color;
+import it.unibo.xiangqi.model.api.Move;
+import it.unibo.xiangqi.model.api.Position;
 import it.unibo.xiangqi.controller.api.InputHandler;
 import it.unibo.xiangqi.model.api.Board;
 
