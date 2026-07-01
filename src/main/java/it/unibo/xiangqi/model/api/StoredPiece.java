637d9cead733f22e63bf6e2a2d30752d089c3704
Aicha Nidam <aicha.nidam@studio.unibo.it>
2026-07-01 22:55:31 +0200

    Added the javadoc to StoredPiece

--- src/main/java/it/unibo/xiangqi/model/api/StoredPiece.java
+++ src/main/java/it/unibo/xiangqi/model/api/StoredPiece.java
@@ -3,5 +3,14 @@ package it.unibo.xiangqi.model.api;
 import it.unibo.xiangqi.common.api.PieceType;
 import it.unibo.xiangqi.common.api.Color;
 
-public record StoredPiece(PieceType type, Color color, Position position) {
+/**
+ * Represents the data required to recreate a piece when restoring
+ * a previously saved game
+ * 
+ * @param type the type of the piece
+ * @param color the color of the owning player
+ * @param position the position of the piece on the board
+ */
+
+public record StoredPiece(PieceType type, Color color, Position position){
 }
