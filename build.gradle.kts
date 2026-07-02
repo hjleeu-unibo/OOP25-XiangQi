6de1d3e94a62a96befc5487be65752efecfa1e74
Shenghui Hong <shenghui.hong@studio.unibo.it>
2026-07-02 17:28:54 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,32 @@
-plugins { java }
+plugins { 
+    java 
+    application
+}
+
+application{
+    mainClass.set("it.unibo.xiangqi.app.XiangqiApplication")
+}
+
+tasks.javadoc {
+    options.memberLevel = JavadocMemberLevel.PUBLIC
+}
 
 repositories { // Where to search for dependencies
     mavenCentral()
 }
 
 dependencies {
-        // JUnit API and testing engine
-        testImplementation(platform("org.junit:junit-bom:6.0.3"))
-        testImplementation("org.junit.jupiter:junit-jupiter-api")
-        testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-        testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    // JUnit API and testing engine
+    testImplementation(platform("org.junit:junit-bom:6.0.3"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+
+    // Mockito
+    testImplementation("org.mockito:mockito-core:5.15.2")
+    testRuntimeOnly("org.mockito:mockito-junit-jupiter:5.15.2")
 }
 
+tasks.test {
+    useJUnitPlatform()
+}
