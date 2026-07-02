72e0f10d94c2d67cc084a2d2dff01fc4c949639d
Shenghui Hong <shenghui.hong@studio.unibo.it>
2026-07-02 13:07:35 +0200

    Update GameModelImpl, deleting buildPlayers method

--- src/main/java/it/unibo/xiangqi/model/impl/GameModelImpl.java
+++ src/main/java/it/unibo/xiangqi/model/impl/GameModelImpl.java
@@ -169,10 +169,4 @@ public class GameModelImpl implements GameModel {
             blackHints--;
         }
     }
-
-    private void buildPlayers(final GameModeType mode) {
-        final Player red = new PlayerImpl(Color.RED, true);
-        final Player black = new PlayerImpl(Color.BLACK, mode == GameModeType.PVP);
-        this.players = List.of(red, black);
-    }
 }
