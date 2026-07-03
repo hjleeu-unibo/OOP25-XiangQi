4765e65bbe574ce9281eb54949706a0e7ccee818
hjleeu-unibo <haojie.liu@studio.unibo.it>
2026-07-03 02:06:03 +0200

    Fixed known issues from merging

--- src/main/java/it/unibo/xiangqi/model/impl/GameModelImpl.java
+++ src/main/java/it/unibo/xiangqi/model/impl/GameModelImpl.java
@@ -5,7 +5,7 @@ import java.util.List;
 import it.unibo.xiangqi.common.api.Color;
 import it.unibo.xiangqi.common.api.GameModeType;
 import it.unibo.xiangqi.common.api.GameStatus;
-import it.unibo.xiangqi.common.api.StoredPiece;
+import it.unibo.xiangqi.model.api.StoredPiece;
 import it.unibo.xiangqi.model.api.Board;
 import it.unibo.xiangqi.model.api.GameModel;
 import it.unibo.xiangqi.model.api.GameState;
